e5ac91c92b7c2ef1f762d65dc00f97a307268832
Amar Krishna <amar.pc.work@gmail.com>
2026-06-08 12:13:18 +0530

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        position_speed_dict = {}
+        for i in range(len(position)):
+            position_speed_dict[position[i]] = speed[i]
+
+        position.sort()
+        time_stack = []
+        for j in range(len(position)):
+            time = float((target - position[j])/position_speed_dict[position[j]])
+            time_stack.append(time)
+        
+        prev = float('-inf')
+        fleet_count = 0
+        while time_stack:
+            current_time = time_stack.pop()
+            if current_time > prev:
+                fleet_count += 1
+                prev = current_time
+
+
+        return fleet_count
+                 
+
+
+
+            
+
+            
+
+
+        
